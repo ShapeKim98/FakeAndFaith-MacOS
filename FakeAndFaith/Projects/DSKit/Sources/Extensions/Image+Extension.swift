7ca60ddd75929990551fe9ee118e5b41dcea3682
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-16 19:36:33 +0900

    [feat] #3 이미지 연결

---
+++ FakeAndFaith/Projects/DSKit/Sources/Extensions/Image+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Image+Extension.swift
+//  DSKit
+//
+//  Created by 김도형 on 6/16/24.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let arrowRightIcon = DSKitAsset.arrowRightIcon.swiftUIImage
+    static let eyeIcon = DSKitAsset.eyeIcon.swiftUIImage
+    static let nextIcon = DSKitAsset.eyeIcon.swiftUIImage
+    static let pauseIcon = DSKitAsset.pauseIcon.swiftUIImage
+    static let playIcon = DSKitAsset.playIcon.swiftUIImage
+    static let xIcon = DSKitAsset.xIcon.swiftUIImage
+    static let earImaage = DSKitAsset.earImage.swiftUIImage
+    static let eyeImage = DSKitAsset.eyeImage.swiftUIImage
+    static let handImage = DSKitAsset.handImage.swiftUIImage
+    static let peopleImage = DSKitAsset.peopleImage.swiftUIImage
+    static let eyeSymbol = DSKitAsset.eyeSymbol.swiftUIImage
+    static let earSymbol = DSKitAsset.earSymbol.swiftUIImage
+    static let handSymbol = DSKitAsset.handSymbol.swiftUIImage
+    static let logo = DSKitAsset.logo.swiftUIImage
+}
