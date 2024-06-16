e214509ea82d13d73bbb23aeda5d0cbd54b56554
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-16 19:22:43 +0900

    [feat] #1 리소스 파일 추가 및 디자인시스템 구축

---
+++ FakeAndFaith/Projects/DSKit/Sources/Extensions/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  DSKit
+//
+//  Created by 김도형 on 6/16/24.
+//
+
+import SwiftUI
+
+public extension Color {
+    static let main = DSKitAsset.main.swiftUIColor
+}
