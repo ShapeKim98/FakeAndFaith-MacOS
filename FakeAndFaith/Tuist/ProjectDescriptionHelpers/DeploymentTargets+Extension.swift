d18a1a0b49879a0d8f23c3a97a3f29ec0f91d798
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-16 16:44:13 +0900

    [feat[ #1 Tuist 구축

---
+++ FakeAndFaith/Tuist/ProjectDescriptionHelpers/DeploymentTargets+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  DeploymentTarget+Extension.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 김도형 on 6/12/24.
+//
+
+import ProjectDescription
+
+public extension DeploymentTargets {
+    static let appMinimunTarget: DeploymentTargets = .iOS("16.0")
+}
