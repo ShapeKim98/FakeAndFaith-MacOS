e7bc28d20ae0d8a26f0988a7f9b32b5128434afd
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-09-08 17:52:37 +0900

    [fix] #11 ci_script 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,10 +8,24 @@
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
-curl https://mise.jdx.dev/install.sh | sh
+set -e
+cd ..
 
-mise install
+curl https://mise.run | sh
+export PATH="$HOME/.local/bin:$PATH"
 
-mise x tuist clean
-mise x tuist install
-mise x tuist generate
+# Output the current PATH for debugging
+echo "❗️Current PATH: $PATH"
+
+echo "❗️mise version"
+mise --version
+echo "❗️mise install"
+mise install # Installs the version from .mise.toml
+eval "$(mise activate bash --shims)"
+
+echo "❗️mise doctor"
+mise doctor # verify the output of mise is correct on CI
+echo "❗️tuist install"
+tuist install
+echo "❗️tuist generate"
+tuist generate # Generate the Xcode Project using Tuist
